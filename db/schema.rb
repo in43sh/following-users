f7cdb01ccdaf5bf362341dfec31fd035c6ccd3fe
Sergey Sherstobitov <in43sh@gmail.com>
2018-12-13 03:11:17 -0800

    working on followers and followings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_221332) do
+ActiveRecord::Schema.define(version: 2018_12_13_110152) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_12_11_221332) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
