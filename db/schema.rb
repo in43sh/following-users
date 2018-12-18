c42c8556898562af77d172c60a5dd42edc3d730b
Sergey Sherstobitov <in43sh@gmail.com>
2018-12-18 03:37:50 -0800

    fixing bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_13_110152) do
+ActiveRecord::Schema.define(version: 2018_12_18_113230) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 2018_12_13_110152) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
